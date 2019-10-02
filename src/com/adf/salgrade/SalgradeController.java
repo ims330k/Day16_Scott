911583a6306e2fba0a2e1f49b5dcc639dbf3ba3e
ims330k@gmail.com <55524892+ims330k@users.noreply.github.com>
2019-10-02 18:31:35 +0900

    SALCONTROLLER(3)

--- src/com/adf/salgrade/SalgradeController.java
+++ src/com/adf/salgrade/SalgradeController.java
@@ -9,52 +9,38 @@ public class SalgradeController {
 
 		Scanner sc=new Scanner(System.in);
 		SalgradeDAO sdao=new SalgradeDAO();
-		salgradeView sv=new salgradeView();
-		salgradeInput si=new salgradeInput();
+		SalgradeView sv=new SalgradeView();
+		SalgradeInput si=new SalgradeInput();
+		
 		
 		
 		boolean check=true;
 
 		while(check) {
+			ArrayList<SalDTO> sdtoList=null;
 			System.out.println("1.전체조회 2.검색조회 3.추가 4.삭제");
 			int select=sc.nextInt();
 			int result=0;
 			switch(select) {
 			case 1:
 				System.out.println("전체조회");
-				ArrayList<SalDTO> sdtoList=null;
 				sdtoList=sdao.selectAll();
-				//salgradeView.view(sdtoList);
+				sv.view(sdtoList);
 				break;
 			case 2:
 				System.out.println("검색할 번호 입력");
 				int searchNum=sc.nextInt();//int매개변수, return sdto
 				SalDTO sdto=new SalDTO();
 				sdto=sdao.selectOne(searchNum);
-				//salgradeView.view(sdto);
+				sv.view(sdto);
 				break;
 			case 3:
-				System.out.println("추가");
-				System.out.println("grade : ");
-				int grade=sc.nextInt();
-				System.out.println("losal : ");
-				int losal=sc.nextInt();
-				System.out.println("hisal : ");
-				int hisal=sc.nextInt();
-				SalDTO sdto2=new SalDTO();
-				sdto2.setGrade(grade);
-				sdto2.setLosal(losal);
-				sdto2.setHisal(hisal);
-				result=sdao.insert(sdto2);
-				if(result>0) {
-					System.out.println("성공");
-				}else {
-					System.out.println("실패");
-				}
-				break;
+				
+				sdto=si.salInsert();
+				sdtoList.add(sdto);
+
 			case 4:
-				System.out.println("삭제할 grade 입력");
-				int num=sc.nextInt();
+				int num=si.salDelete();
 				result=sdao.delete(num);
 				if(result>0) {
 					System.out.println("삭제 성공");
